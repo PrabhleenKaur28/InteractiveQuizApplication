0cc5ed116653fe117638383b972369a5b75b5b1e
Prabhleen28 <prabhleen544@gmail.com>
2024-02-17 22:01:33 +0530

    added files

---
+++ questions.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which planet is called the Red Planet?",
+    answer: "B. MARS",
+    options: [
+      "A. EARTH",
+      "B. MARS",
+      "C. JUPITER",
+      "D. VENUS"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is full of holes but still holds water?",
+    answer: "C. SPONGE",
+    options: [
+      "A. BUCKET",
+      "B. PLATE",
+      "C. SPONGE",
+      "D. GALORE"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What can you catch but not throw?",
+    answer: "B. COLD",
+    options: [
+      "A. BALL",
+      "B. COLD",
+      "C. BIRD",
+      "D. TENNIS"
+    ]
+  },
+    {
+    numb: 4,
+    question: "I belong to you but others use me more than you do. What am I?",
+    answer: "D. NAME",
+    options: [
+      "A. BRAIN",
+      "B. SHADOW",
+      "C. HAND",
+      "D. NAME"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What goes up and down but doesn't move?",
+    answer: "B. ROAD",
+    options: [
+      "A. WATER",
+      "B. ROAD",
+      "C. CLOUDS",
+      "D. WIND"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What does HTML stand for?",
+    answer: "C. Hyper Text Markup Language",
+    options: [
+      "A. Hyper Tools Making Logerithm",
+      "B. Hyper Text Making Language",
+      "C. Hyper Text Markup Language",
+      "D. Helping Tools Making Language"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What is the largest planet in our solar system?",
+    answer: "C. Jupiter",
+    options: [
+      "A. Venus",
+      "B. Earth",
+      "C. Jupiter",
+      "D. Uranus"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Who is known as the 'Father of Modern Physics'?",
+    answer: "C. Albert Einstein",
+    options: [
+      "A. Isaac Newton",
+      "B. Galileo Galilei",
+      "C. Albert Einstein",
+      "D. Nikola Tesla",
+    ]
+  },
+  {
+    numb: 9,
+    question: "What is the capital of Australia?",
+    answer:"C. Canberra",
+    options: [
+      "A. Sydney",
+      "B. Melbourne",
+      "C. Canberra",
+      "D. Brisbane",
+    ]
+  },
+  {
+    numb: 10,
+    question: "In which year did Christopher Columbus first reach the Americas?",
+    answer: "B. 1492",
+    options: [
+      "A. 1401",
+      "B. 1492",
+      "C. 1520",
+      "D. 1607"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
